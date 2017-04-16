3d096c980338438dbd319a45ef60d648c02d4313
Vineetls <vineet@mudupalli.com>
2017-04-16 02:23:09 -0700

    Sends data to app, samples 5 times a second

--- accelLogPoster.c
+++ accelLogPoster.c
@@ -2,7 +2,7 @@
  
 static Window* window;
 static TextLayer   *accel_layer;
- 
+ bool message = true;
 static void window_load(Window *window)
 {
 
@@ -31,12 +31,49 @@ static void accel_raw_handler(AccelData *data, uint32_t num_samples)
   int16_t x = data[0].x;
   int16_t y = data[0].y;
   int16_t z = data[0].z;
+    // Largest expected inbox and outbox message sizes
+const uint32_t inbox_size = 32;
+const uint32_t outbox_size = 32;
+
+// Open AppMessage
+app_message_open(inbox_size, outbox_size);
+
+  
+  // Declare the dictionary's iterator
+DictionaryIterator *out_iter;
+
+// Prepare the outbox buffer for this message
+AppMessageResult result = app_message_outbox_begin(&out_iter);
+if(result == APP_MSG_OK) {
+  // Add an item to ask for weather data
+  
+  dict_write_int16(out_iter, 1 , x);
+  dict_write_int16(out_iter, 2 , y);
+  dict_write_int16(out_iter, 3 , z);
+
+  // Send this message
+  result = app_message_outbox_send();
+  if(result != APP_MSG_OK) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Error sending the outbox: %d", (int)result);
+  }
+} else {
+  // The outbox cannot be used right now
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Error preparing the outbox: %d", (int)result);
+}
+  dict_write_end(out_iter);
   snprintf(buffer, sizeof("XYZ: 9999 / 9999 / 9999"), "XYZ: %d / %d / %d", x, y, z);
   APP_LOG(APP_LOG_LEVEL_INFO, "x: %d, y: %d, z: %d",
                                                           x, y, z);
   text_layer_set_text(accel_layer, buffer);
-}
+  message = false;
+
  
+}
+//  static void inbox_received_callback(DictionaryIterator *iter, void *context) {
+//   // A new message has been successfully received
+// message = true;
+//   accel_data_service_subscribe(1, accel_raw_handler);
+// }
 static void init()
 {
   window = window_create();
@@ -46,12 +83,11 @@ static void init()
   };
   window_set_window_handlers(window, (WindowHandlers) handlers);
   window_stack_push(window, true);
-
+//  app_message_register_inbox_received(inbox_received_callback);
   
-  //Subscribe to AccelerometerService (uncomment one to choose)
-  //accel_tap_service_subscribe(accel_tap_handler);
-  accel_data_service_subscribe(1, accel_raw_handler);
-  accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
+  accel_data_service_subscribe(5, accel_raw_handler);
+
+ // accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
 }
  
 static void deinit()
